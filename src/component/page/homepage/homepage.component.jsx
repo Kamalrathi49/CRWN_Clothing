1426d21fcbba4d096d0fa42de2d8a0afa792756b
Kamal Rathi <kamalrathi049@gmail.com>
2020-12-29 20:56:50 +0530

    creating e-commerce website using Reactjs

---
+++ src/component/page/homepage/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import  "./homepage.style.scss";
+
+
+const HomePage = () =>(
+    <div className='homepage'>
+         <div className='directory-menu'>
+
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>hats </h1>
+                <spam className='subtitle'>shop now</spam>
+              </div>
+            </div>
+          
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>jackets </h1>
+                <spam className='subtitle'>shop now</spam>
+              </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>sneakers </h1>
+                <spam className='subtitle'>shop now</spam>
+              </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>womens </h1>
+                <spam className='subtitle'>shop now</spam>
+              </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                <h1 className='title'>mens</h1>
+                <spam className='subtitle'>shop now</spam>
+              </div>
+            </div>
+
+          </div>
+         </div>
+    )
+
+
+export default HomePage;
