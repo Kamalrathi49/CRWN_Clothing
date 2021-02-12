1b0ee148824707bb7692824cc5d7fa585004bf37
Kamal Rathi <kamalrathi049@gmail.com>
2021-02-12 18:18:42 +0530

    add persist in our application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./index.css";
+
 import App from "./App";
+
 import ReactDOM from "react-dom";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { persistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <persistGate persistor={persistor}>
+        <App />
+      </persistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
