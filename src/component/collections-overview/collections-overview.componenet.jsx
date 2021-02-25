f33a29f20a71672620955dcfbc66ab1ba39cd0f7
Kamal Rathi <kamalrathi049@gmail.com>
2021-02-25 19:16:01 +0530

    fixing an error

--- src/component/collections-overview/collections-overview.componenet.jsx
+++ src/component/collections-overview/collections-overview.componenet.jsx
@@ -5,11 +5,13 @@ import { createStructuredSelector } from "reselect";
 import { selectCollections } from "../../redux/shop/shop.selectors";
 import PreviewCollection from "../preview-collection/preview-collection.component";
 
+
+
 import "./collections-overview.style.scss";
 
 const collectionsOverview = ({ collections }) => (
   <div className="collections-overview">
-    {collections.map(({ id, ...otherCollectionProps }) => (
+    {collections(({ id, ...otherCollectionProps }) => (
       <PreviewCollection key={id} {...otherCollectionProps} />
     ))}
   </div>
