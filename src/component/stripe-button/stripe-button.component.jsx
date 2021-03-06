309402bc6e9889f8141fde1c8a324a46770e0154
Kamal Rathi <kamalrathi049@gmail.com>
2021-03-06 13:41:44 +0530

    addind payment method

--- src/component/stripe-button/stripe-button.component.jsx
+++ src/component/stripe-button/stripe-button.component.jsx
@@ -5,11 +5,28 @@ import "./stripe-button.style.scss";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = "pk_test_51IRdVTKILFNXBNL58G1mk7pG55aV0NZeq0D45xDS7rN78gyLmNrYh1ymRce07pdfIAQPpeTrGsq9axkT8z5Q56EF00HZVDyCvY";
+  const publishableKey =
+    "pk_test_51IRdVTKILFNXBNL58G1mk7pG55aV0NZeq0D45xDS7rN78gyLmNrYh1ymRce07pdfIAQPpeTrGsq9axkT8z5Q56EF00HZVDyCvY";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
 
   return (
-      <StripeCheckout
-      />
-  )
-
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
+
+export default StripeCheckoutButton;
