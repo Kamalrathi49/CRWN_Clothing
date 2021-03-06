a2d8059f0f515fe6138e7b798b985e34aff95d32
Kamal Rathi <kamalrathi049@gmail.com>
2021-03-06 13:06:54 +0530

    addind payment method

--- src/component/stripe-button/stripe-button.component.jsx
+++ src/component/stripe-button/stripe-button.component.jsx
@@ -6,4 +6,10 @@ import "./stripe-button.style.scss";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_51IRdVTKILFNXBNL58G1mk7pG55aV0NZeq0D45xDS7rN78gyLmNrYh1ymRce07pdfIAQPpeTrGsq9axkT8z5Q56EF00HZVDyCvY";
+
+  return (
+      <StripeCheckout
+      />
+  )
+
 };
